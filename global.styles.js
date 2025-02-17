c9733f93725b09835201ef325aa3f680d7a90610
Oleksandra Laposhko <laposhko0@gmail.com>
2025-02-17 23:49:08 +0200

    fixing scss and imports

--- global.styles.js
+++ global.styles.js
@@ -1,10 +1,10 @@
-const variablesStyles = '@import "./src/scss/common/_vars.scss"';
-const breakpointsStyles = '@import "./src/scss/common/_breakpoints.scss"';
-const mixinsStyles = '@import "./src/scss/common/_mixins.scss"';
-const resetStyles = '@import "./src/scss/common/_reset.scss"';
+// const variablesStyles = '@use "./src/scss/common/_vars.scss"';
+const breakpointsStyles = '@use  "./src/scss/common/_breakpoints.scss"';
+const mixinsStyles = '@use "./src/scss/common/_mixins.scss"';
+const resetStyles = '@use "./src/scss/common/_reset.scss"';
 
 const styles = [
-  variablesStyles,
+  // variablesStyles,
   breakpointsStyles,
   //   colorsStyles,
   mixinsStyles,
